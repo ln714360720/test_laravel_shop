7f5a146fa70ac26a96d831eb81776ab3b131ac2f
ln8198 <ln714360720@163.com>
2018-12-11 14:30:21 +0800

    用户注册时激活发送邮箱

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Providers;
-
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -13,8 +12,11 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+//        'App\Events\Event' => [
+//            'App\Listeners\EventListener',
+//        ],
+        'Illuminate\Auth\Events\Registered'=>[
+            'App\Listeners\RegisteredListener'
         ],
     ];
 
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 
 
+use function foo\func;
 
 Route::get("/",'PagesController@root')->name('root');//name为路由命名,为生成url和重定向提供方便
 //自带的登录注册  路由信息在vendor/laravel/framework/src/Illuminate/Routing/Router.php里的auth方法里
@@ -20,6 +21,11 @@ Auth::routes();//
 Route::group(['middleware'=>'auth'],function (){
     //验证邮箱路由
    Route::get("email_verify_notice",'PagesController@emailVerifyNotice')->name('email_verify_notice');
-   
+   Route::get('email_verification/verify','EmailVerificationController@verify')->name('email_verification.verify');
+   Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');//用户主动发送邮件
+    //email_verified这个中间件是自己定义的,只有当邮箱验证通过了,才执行以下方法
+    Route::group(['middleware'=>'email_verified'],function (){
+    
+    });
 });
 
