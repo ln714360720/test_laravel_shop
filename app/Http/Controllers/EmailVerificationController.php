c0e5d07b26b4bae2d6a5c7b94762363f3cf0f8b5
ln8198 <ln714360720@163.com>
2018-12-17 10:40:15 +0800

    用户列表

---
+++ app/Http/Controllers/EmailVerificationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\InvalidRequestException;
+use App\Models\User;
+use App\Notifications\EmailVerificationNotification;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class EmailVerificationController extends Controller
+{
+    /**验证用户邮箱
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Exception
+     */
+    public function verify(Request $request)
+    {
+        $email=$request->input('email');
+        $token=$request->input('token');
+        if(!$email || !$token){
+            throw new InvalidRequestException('验证链接不正确');
+        }
+        if($token !=Cache::get('email_verification_'.$email)){
+            throw  new InvalidRequestException('验证链接不正确或已过期!');
+        }
+        if(!$user= User::where('email',$email)->first()){
+            throw new InvalidRequestException('当前用户不存在');
+        }
+        Cache::forget('email_verification_'.$email);
+        $user->update(['email_verified'=>true]);
+        return view('pages.success',['msg'=>'邮箱验证成功!']);
+    }
+    
+    /**用户主动点击发送邮件
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Exception
+     */
+    public function send(Request $request)
+    {
+        $user=$request->user();
+        if($user->email_verified){
+            throw new InvalidRequestException('你已经验证过邮箱了',400);
+        }
+        $user->notify(new EmailVerificationNotification());
+        return view('pages.success',['msg'=>'邮件发送成功']);
+    }
+}
