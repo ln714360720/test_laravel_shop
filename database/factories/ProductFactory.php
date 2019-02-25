02e494dba1730595eef567775319bc7956e508f6
ln8198 <ln714360720@163.com>
2019-02-25 14:56:27 +0800

    优化前台商品列表可以按分类搜索

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Models\Category;
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Product::class, function (Faker $faker) {
+    //从数据库里随机取出一个类目
+    $category=Category::query()->where('is_directory',false)->inRandomOrder()->first();
     $image=$faker->randomElement([
         "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/7kG1HekGK6.jpg",
         "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/1B3n0ATKrn.jpg",
@@ -24,5 +27,6 @@ $factory->define(App\Models\Product::class, function (Faker $faker) {
         'sold_count'=>0,
         'review_count'=>0,
         'price'=>0,
+        'category_id'=>$category?$category->id:null,
     ];
 });
