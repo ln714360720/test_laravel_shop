ffc79d01270d7f0a36a56538df283dcf0a577e5c
ln8198 <ln714360720@163.com>
2019-03-15 14:23:53 +0800

    同步商品数据到Elasticsearch

---
+++ app/Console/Commands/Elasticsearch/SyncProducts.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands\Elasticsearch;
+
+use App\Models\Product;
+use Illuminate\Console\Command;
+
+class SyncProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'es:sync-products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '将商品数据同步到Elasticsearch';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //获取Elasticsearch对象
+        $es=app('es');
+        Product::query()->with(['skus','properties'])
+            ->chunkById(100, function($products)use ($es){
+               $this->info(sprintf('正在同步ID范围为%s至%s的商品数据',$products->first()->id,$products->last()->id));
+               //初始化请求体
+                $req=['body'=>[]];
+                //遍历商品
+                foreach ($products as $product) {
+                    //将商品模型转化为数组
+                    $data=$product->toESArray();
+                    $req['body'][]=[
+                      'index'=>[
+                          '_index'=>'products',
+                          '_type'=>'_doc',
+                          '_id'=>$data['id'],
+                      ],
+                    ];
+                    $req['body'][]=$data;
+                }
+                
+                try{
+                   
+                    //使用bulk方法批量创建
+                    $es->bulk($req);
+                    }catch(\Exception $e){
+                        $this->info($e->getMessage());
+                    }
+            });
+        $this->info('同步完成');
+    }
+}
